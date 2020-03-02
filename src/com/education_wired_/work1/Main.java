83ff324f31821a2d1349eb03a4657eb1be92d4b9
AnatolyParkhomenko <tolyeg@gmail.com>
2020-03-02 20:46:37 +0200

    02-03-2020

--- src/com/education_wired_/work1/Main.java
+++ src/com/education_wired_/work1/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        MyPhoneBook phoneBook = new MyPhoneBook();
+        phoneBook.addRecord("tom","555-55-55");
+        phoneBook.addRecord("jerry","555-55-66");
+        phoneBook.addRecord("baloo","555-55-77");
+        phoneBook.addRecord("tiger","555-55-88");
+        phoneBook.addRecord("rabbit","555-55-99");
+        phoneBook.addRecord("kenga","555-66-99");
+        phoneBook.addRecord("bamby","555-77-99");
+
+        phoneBook.printPhoneRecords();
     }
 }
